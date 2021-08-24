f29c207c35fe97f382013c58f5eb46527cc4a8d0
Florian Delrieu <florian.delrieu@free.fr>
2021-08-24 13:35:44 +0200

    add Walker unit

--- W40K_addon/W40K_Unit.py
+++ W40K_addon/W40K_Unit.py
@@ -83,6 +83,9 @@ class Unit:
     def set_Quantity(self,Quantity):
         self.Quantity = Quantity
 
+########################################################################################################################
+########################################################################################################################
+
 class Tank:
     def __init__(self,CT=3,Blind_Av=12,Blind_Side=11,Blind_Arr=10,PC=3,Quantity=1):
         self.Quantity = Quantity
@@ -110,6 +113,66 @@ class Tank:
         self.ORG = 2*self.Quantity
         self.SoftMeleeAttack = 0
         self.HardMeleeAttack = 0
+    # Hardness & Armor
+        self.Armor = np.mean(self.Blind_Av,self.Blind_Side,self.Blind_Arr)
+    def round_Stats(self):
+        self.Defense = round(self.Defense,2)
+        self.Breakthrought = round(self.Breakthrought,2)
+        self.SoftAttack = round(self.SoftAttack,2)
+        self.HardAttack = round(self.HardAttack,2)
+        self.SoftMeleeAttack = round(self.SoftMeleeAttack,2)
+        self.HardMeleeAttack = round(self.HardMeleeAttack,2)
+    def Show_HOI_Stats(self):
+        self.HOI4_Profil()
+        txt = """
+        HP              = {}
+        ORG             = {}
+        Defense         = {}   
+        Breakthrought   = {}
+        SoftMA          = {}
+        HardMA          = {}
+        Hardness        = {}
+        Armor           = {}
+        """.format(self.HP,self.ORG,
+                   self.Defense,self.Breakthrought,
+                   self.SoftMeleeAttack,self.HardMeleeAttack,
+                   self.Hardness,self.Armor)
+        print(txt)
+    def set_Quantity(self,Quantity):
+        self.Quantity = Quantity
+
+########################################################################################################################
+########################################################################################################################
+
+class Walker:
+    def __init__(self,CC=3,CT=3,F=5,Blind_Av=12,Blind_Side=11,Blind_Arr=10,A=2,PC=3,Quantity=1):
+        self.Quantity = Quantity
+    # W40K Stats
+        self.CC = CC
+        self.CT = CT
+        self.F = F
+        self.Blind_Av = Blind_Av
+        self.Blind_Side = Blind_Side
+        self.Blind_Arr = Blind_Arr
+        self.A = A
+        self.PC = PC
+        # HOI Stats
+        self.HP = float()
+        self.ORG = float()
+        self.SoftAttack = float()
+        self.HardAttack = float()
+        self.Defense = float()
+        self.Breakthrought = float()
+        self.SoftMeleeAttack = float()
+        self.HardMeleeAttack = float()
+        self.Hardness = float()
+        self.Armor = float()
+        self.Piercing = float()
+    def HOI4_Profil(self):
+        self.HP = self.PC * self.Quantity
+        self.ORG = 1*self.Quantity
+        self.SoftMeleeAttack = self.A*SoftAttack_CC_CT[self.CC]*self.Quantity
+        self.HardMeleeAttack = HMA_SMA_prop[self.F]*self.SoftMeleeAttack*HardAttack_CC_CT[self.CC]*self.Quantity
     # Hardness & Armor
         self.Armor = np.mean(self.Blind_Av,self.Blind_Side,self.Blind_Arr)
     def round_Stats(self):
