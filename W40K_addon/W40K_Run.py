b379bceaada96071571768feda38da8ea70c233d
Florian Delrieu <florian.delrieu@free.fr>
2021-08-26 14:17:47 +0200

    test company 2
    Space marines company
    - 100 bolter
    - 95 bolter & 5 Plasma

--- W40K_addon/W40K_Run.py
+++ W40K_addon/W40K_Run.py
@@ -4,20 +4,20 @@ from W40K_Company import *
 from W40K_Unit import *
 
 Bolter = Weapon(4,5,"Tir rapide")
+PlasmaGun = Weapon(7,2,"Tir Rapide")
 SpaceMarines = Unit(4,4,4,5,1,1,7,3)
 
 
 Comp1 = Company()
-SpaceMarines.set_Quantity(10)
-Bolter.set_Quantity(10)
+SpaceMarines.set_Quantity(100)
+Bolter.set_Quantity(100)
 Comp1.setUnit(SpaceMarines)
 Comp1.setEquipement([Bolter])
 
 Comp2 = Company()
-SpaceMarines.set_Quantity(100)
-Bolter.set_Quantity(100)
+Bolter.set_Quantity(95)
 Comp2.setUnit(SpaceMarines)
-Comp2.setEquipement([Bolter])
+Comp2.setEquipement([Bolter,PlasmaGun])
 
 
 Comp1.Show_HOI_Stats()
