7ddfec6c151708ff692528758a49f971824390f1
Florian Delrieu <florian.delrieu@free.fr>
2021-08-28 22:32:21 +0200

    Wrong Bolter F

--- W40K_addon/W40K_Run.py
+++ W40K_addon/W40K_Run.py
@@ -3,7 +3,7 @@ from W40K_addon.W40K_Company import *
 
 
 SpaceMarines = Unit(4,4,4,4,1,1,8,3,None,100)
-Bolter = Weapon(F=3,PA=5,Type="Tir Rapide",Quantity=100)
+Bolter = Weapon(F=4,PA=5,Type="Tir Rapide",Quantity=100)
 ReacteurDorsaux = Upgrade(BRK=1.5,Quantity = 20)
 
 Comp = Company()
