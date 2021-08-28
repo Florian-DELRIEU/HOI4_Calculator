70a71a350cec27a5ae33ca01d35cfc1b9a996df7
Florian Delrieu <florian.delrieu@free.fr>
2021-08-28 12:34:22 +0200

    #12 test B and minor fix
    - add CCW test

--- W40K_addon/W40K_Run.py
+++ W40K_addon/W40K_Run.py
@@ -6,8 +6,9 @@ from W40K_Unit import *
 Guards = Unit(Quantity=100)
 Lasgun = Weapon(3,None,"Tir Rapide",Quantity=90,Name="Lasgun")
 PlasmaGun = Weapon(7,2,"Tir Rapide",Quantity=5,Name="PlasmaGun")
-BolterLD = Weapon(5,4,"Lourd",Quantity=5,Range=36,Name="Bolter Lourds")
+BolterLD = Weapon(5,4,"Lourde",Quantity=5,Range=36,Name="Bolter Lourds")
+CCW = Weapon(Guards.F+1,4,"Melee",Name="CCW",Quantity=20)
 
 Comp1 = Company()
 Comp1.setUnit(Guards)
-Comp1.setEquipement([Lasgun,PlasmaGun,BolterLD])
+Comp1.setEquipement([Lasgun,PlasmaGun,BolterLD,CCW])
