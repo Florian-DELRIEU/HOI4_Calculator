906981915db7c2dcd0ad5d3597f30e17c2818628
floriandelrieu <florian.delrieu@free.fr>
2021-08-29 20:31:40 +0200

    add Documentation for W40K_Bonus

--- W40K_addon/W40K_Bonus.py
+++ W40K_addon/W40K_Bonus.py
@@ -1,6 +1,11 @@
 from W40K_addon.W40K_Company import *
 
 def setWeaponsBonus(Object):
+    """
+    Verifie les règles spéciales des :class Weapons: pour ajouté les bonus
+    :param Object: class weapons
+    """
+    assert type(Object) == Weapon , "Object must be :class Weapon:"
 # Weapons Specials rules
     for rule in Object.SpecialsRules:
         if rule == "Perforant":
@@ -29,7 +34,7 @@ def setWeaponsBonus(Object):
         Object.Defense *= 1.1
         Object.Breakthrought = 1.1
 # Weapon Range
-    if Object.Range == (0 or None): pass
+    if Object.Range == (0 or None): pass # melee weapons
     if Object.Range <= 8:
         Object.SoftAttack *= 0.4
         Object.HardAttack *= 0.4
@@ -50,9 +55,15 @@ def setWeaponsBonus(Object):
         Object.HardAttack *= 1.2
         Object.Defense *= 1.2
         Object.Breakthrought *= 1.2
+    elif Object.Range >= 36:
+        Object.SoftAttack *= 1.4
+        Object.HardAttack *= 1.4
+        Object.Defense *= 1.4
+        Object.Breakthrought *= 1.4
 
 def setUpgradeBonus(company):
-    #assert type(company) is Company, "Argument must be a company"
+    assert type(company) == Company, "Argument must be a company"
+# Coefs des bonus à 1 de base
     SoftAttack_Bonus = 1
     HardAttack_Bonus = 1
     SoftMeleeAttack_Bonus = 1
@@ -60,7 +71,7 @@ def setUpgradeBonus(company):
     Defense_Bonus = 1
     Breakthrought_Bonus = 1
     for el in company.Upgrade: # Additionne tout les bonus des Upgrades
-        equip_ratio = el.Quantity/company.Manpower
+        equip_ratio = el.Quantity/company.Manpower  # quantité d'upgrade par rapport au Manpower
     # Cette opération est pour incorporer le :equip_ratio: dans les bonus
         SoftAttack_Bonus *= 1 + (el.SoftAttack_Bonus-1)*equip_ratio
         HardAttack_Bonus *= 1 + (el.HardAttack_Bonus-1)*equip_ratio
@@ -68,6 +79,7 @@ def setUpgradeBonus(company):
         HardMeleeAttack_Bonus *= 1 + (el.HardMeleeAttack_Bonus-1)*equip_ratio
         Defense_Bonus *= 1 + (el.Defense_Bonus-1)*equip_ratio
         Breakthrought_Bonus *= 1 + (el.Breakthrought_Bonus-1)*equip_ratio
+# Application des bonus
     company.SoftAttack = company.SoftAttack*SoftAttack_Bonus
     company.HardAttack = company.HardAttack*HardAttack_Bonus
     company.SoftMeleeAttack = company.SoftMeleeAttack*SoftMeleeAttack_Bonus
