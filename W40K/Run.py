5355f0c266fd3eea14e66a7dab4baec3905a6fe6
Florian Delrieu <florian.delrieu@free.fr>
2021-09-14 22:01:12 +0200

    add Save Companies

--- W40K/Run.py
+++ W40K/Run.py
@@ -1,21 +1,11 @@
-from W40K.UnitCreator.Unit_list import *
-from W40K.LandBattles.Company import Company,Regiment
+from W40K.LandBattles.Companies_list import *
+from W40K.LandBattles.Company import Regiment
 from W40K.LandBattles.Battle import Battle
 
-CompA = Company(Unit=Gardes.__copy__(100),
-                Equipement=[Lasgun.__copy__(95),
-                            PlasmaGun.__copy__(5),
-                            FragGrenades.__copy__(100),
-                            KrakGrenades.__copy__(100)])
-RegA = Regiment([CompA,CompA,CompA,CompA],"101st Regiment")
+RegA = Regiment([Guardsmens_100A,Guardsmens_100A,Guardsmens_100A,Guardsmens_100A],"101st Regiment")
+RegA.isDefending = True
 
-CompB = Company(Unit=SpaceMarines.__copy__(100),
-                Equipement=[Bolter.__copy__(90),
-                            PlasmaGun.__copy__(5),
-                            BolterLD.__copy__(5),
-                            FragGrenades.__copy__(100),
-                            KrakGrenades.__copy__(100)])
-RegB = Regiment([CompB,CompB],"Ultramarines")
+RegB = Regiment([Spaces_100A,Spaces_100A],"Ultramarines")
 RegB.isDefending = False
 
 BattleA = Battle(RegB,RegA)
--- W40K/Saves/Saver.py
+++ W40K/Saves/Saver.py
@@ -2,26 +2,35 @@
 Extraits les :class Unit et Weapons: depuis le fichier :Unit_list: pour sauvegarder leurs profil HOI IV dans deux fichiers CSV distincs 
 """
 from MyPack.Convert import Dict2CSV
-import W40K.UnitCreator.Unit_list as main
+import W40K.LandBattles.Companies_list as CompanyList
+import W40K.UnitCreator.Unit_list as UnitsWeapons_List
 from W40K.UnitCreator.Class.Unit import Infantry,Tank,Walker
 from W40K.UnitCreator.Class.Weapons import Weapon
+from W40K.LandBattles.Company import Company
 
 
-DICO_1 = main.__dict__    # toutes les variables et classes de main dans un dico
-DICO_2A = dict()           # Pour les sauvegardes
-DICO_2B = dict()           # Pour les sauvegardes
+DICO_1A = UnitsWeapons_List.__dict__    # toutes les variables et classes de main dans un dico
+DICO_1B = CompanyList.__dict__    # toutes les variables et classes de main dans un dico
+DICO_2A = dict()           # Units
+DICO_2B = dict()           # Weapons
+DICO_2C = dict()           # Companies
 DICO_3A = dict()
 DICO_3B = dict()
+DICO_3C = dict()
 
 # For UNITS
-for key in DICO_1.keys():
-    if type(DICO_1[key]) in [Infantry, Tank, Walker]:
-        DICO_2A[key] = DICO_1[key]
+for key in DICO_1A.keys():
+    if type(DICO_1A[key]) in [Infantry, Tank, Walker]:
+        DICO_2A[key] = DICO_1A[key]
 # For WEAPONS
-for key in DICO_1.keys():
-    if type(DICO_1[key]) in [Weapon]:
-        DICO_2B[key] = DICO_1[key]
-del DICO_1
+for key in DICO_1A.keys():
+    if type(DICO_1A[key]) in [Weapon]:
+        DICO_2B[key] = DICO_1A[key]
+# For COMPANIES
+for key in DICO_1B.keys():
+    if type(DICO_1B[key]) in [Company]:
+        DICO_2C[key] = DICO_1B[key]
+del DICO_1A , DICO_1B
 
 # Saves UNITS
 fisrt_column_A = ["HP","ORG","SoftAttack","HardAttack","SoftMeleeAttack","HardMeleeAttack","Defense",
@@ -45,5 +54,17 @@ for key in DICO_2B.keys():
             cur.Breakthrought]
     DICO_3B[key] = cur_list
 
+# Saves COMPANIES
+fisrt_column_C = ["HP","ORG","SoftAttack","HardAttack","SoftMeleeAttack","HardMeleeAttack","Defense",
+            "Breakthrought","Armor","Piercing","Hardness"]
+DICO_3C["Companies"] = fisrt_column_C
+for key in DICO_2C.keys():
+    cur = DICO_2C[key]
+    assert type(cur) == Company
+    cur_list = [cur.HP,cur.ORG,cur.SoftAttack,cur.HardAttack,cur.SoftMeleeAttack,cur.HardMeleeAttack,cur.Defense,
+            cur.Breakthrought,cur.Armor,cur.Piercing,cur.Hardness]
+    DICO_3C[key] = cur_list
+
 Dict2CSV(DICO_3A, "SaveUnit.csv")
 Dict2CSV(DICO_3B, "SaveWeapons.csv")
+Dict2CSV(DICO_3C, "SaveCompanies.csv")
