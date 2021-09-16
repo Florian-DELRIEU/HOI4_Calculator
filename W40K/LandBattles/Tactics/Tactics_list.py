66834372d120e1057621a919b37216467ce819c2
Florian Delrieu <florian.delrieu@free.fr>
2021-09-16 14:29:30 +0200

    Minor: Change cac for attack tactic

--- W40K/LandBattles/Tactics/Tactics_list.py
+++ W40K/LandBattles/Tactics/Tactics_list.py
@@ -1,6 +1,6 @@
 from W40K.LandBattles.Tactics.Tactics_class import Tactic
 
-Attack = Tactic(SA=1.05,HA=1.05,CAC=0.1,isDefenseTactic=False,Name="Attaque")
+Attack = Tactic(SA=1.05,HA=1.05,CAC=0,isDefenseTactic=False,Name="Attaque")
 Assaut = Tactic(SA=1.25,HA=1.25,CAC=0.3,isDefenseTactic=False,Name="Assaut")
 Shock = Tactic(BRK=1.25,CAC=0.5,isDefenseTactic=False,Name="Shock")
 
