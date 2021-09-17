7bbe638f3e44e35ed794ad164d6da106e5bc1c86
Florian Delrieu <florian.delrieu@free.fr>
2021-09-17 14:08:23 +0200

    add Bataillon Comparator

--- HOI/Comparator.py
+++ HOI/Comparator.py
@@ -14,8 +14,8 @@ def Analyse_Division(Division):
 def Compare_Division(DivA, DivB):
     txt = "COMPARATOR"
     txt += "=================="
-    txt += "\nHP   |   {}    |   {}  (+ {} %)  ".format(DivA._PVmax, DivB._PVmax, 100*(divide(DivB._PVmax, DivA._PVmax) - 1))
-    txt += "\nORG  |   {}    |   {}  (+ {} %)  ".format(DivA._ORG, DivB._ORG, 100*(divide(DivB._ORG, DivA._ORG) - 1))
+    txt += "\nHP   |   {}    |   {}  (+ {} %)  ".format(DivA.PV, DivB.PV, 100*(divide(DivB.PV, DivA.PV) - 1))
+    txt += "\nORG  |   {}    |   {}  (+ {} %)  ".format(DivA.ORG, DivB.ORG, 100*(divide(DivB.ORG, DivA.ORG) - 1))
     txt += "\nSA   |   {}    |   {}  (+ {} %)  ".format(DivA.SA, DivB.SA, 100*(divide(DivB.SA, DivA.SA) - 1))
     txt += "\nHA   |   {}    |   {}  (+ {} %)  ".format(DivA.HA, DivB.HA, 100*(divide(DivB.HA, DivA.HA) - 1))
     txt += "\nDEF  |   {}    |   {}  (+ {} %)  ".format(DivA.DEF, DivB.DEF, 100*(divide(DivB.DEF, DivA.DEF) - 1))
@@ -40,6 +40,27 @@ def Analyze_Bataillon(Bataillon):
     txt += "\n \n"
     print(txt)
 
+def Compare_Bataillon(BatA, BatB):
+    txt = "COMPARATOR"
+    txt += "=================="
+    txt += "\nHP   |   {}    |   {}  (+ {} %)  ".format(BatA.PV, BatB.PV, 100*(divide(BatB.PV, BatA.PV) - 1))
+    txt += "\nORG  |   {}    |   {}  (+ {} %)  ".format(BatA.ORG, BatB.ORG, 100*(divide(BatB.ORG, BatA.ORG) - 1))
+    txt += "\nSA   |   {}    |   {}  (+ {} %)  ".format(BatA.SA, BatB.SA, 100*(divide(BatB.SA, BatA.SA) - 1))
+    txt += "\nHA   |   {}    |   {}  (+ {} %)  ".format(BatA.HA, BatB.HA, 100*(divide(BatB.HA, BatA.HA) - 1))
+    txt += "\nDEF  |   {}    |   {}  (+ {} %)  ".format(BatA.DEF, BatB.DEF, 100*(divide(BatB.DEF, BatA.DEF) - 1))
+    txt += "\nBRK  |   {}    |   {}  (+ {} %)  ".format(BatA.BRK, BatB.BRK, 100*(divide(BatB.BRK, BatA.BRK) - 1))
+    txt += "\nARM  |   {}    |   {}  (+ {} %)  ".format(BatA.ARM, BatB.ARM, 100*(divide(BatB.ARM, BatA.ARM) - 1))
+    txt += "\nPRC  |   {}    |   {}  (+ {} %)  ".format(BatA.PRC, BatB.PRC, 100*(divide(BatB.PRC, BatA.PRC) - 1))
+    txt += "\nHRD  |   {}    |   {}  (+ {} %)  ".format(BatA.HARD, BatB.HARD, 100*(divide(BatB.HARD, BatA.HARD) - 1))
+    txt += "\nIC   |   {}    |   {}  (+ {} %)  ".format(BatA.IC, BatB.IC, 100*(divide(BatB.IC, BatA.IC) - 1))
+    txt += "\nSupply   |   {}    |   {}  (+ {} %)  ".format(BatA.Supply_use, BatB.Supply_use,
+                                                            100*(divide(BatB.Supply_use, BatA.Supply_use) - 1))
+    txt += "\nFuel   |   {}    |   {}  (+ {} %)  ".format(BatA.Fuel_use, BatB.Fuel_use,
+                                                            100*(divide(BatB.Fuel_use, BatA.Fuel_use) - 1))
+
+    txt +="\n \n"
+    print(txt)
+
 ##### TESTING
 Analyse_Division(Infantery_72)
 Analyse_Division(Armored_432)
@@ -48,3 +69,4 @@ Compare_Division(Infantery_72,Armored_432)
 
 Analyze_Bataillon(CAV_36)
 Analyze_Bataillon(M_INF_36)
+Compare_Bataillon(CAV_36,M_INF_36)
