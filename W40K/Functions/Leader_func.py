5a081960f3c0cb3e0f6e5f51def99d53faf559cd
Florian DELRIEU <47951736+Florian-DELRIEU@users.noreply.github.com>
2022-01-09 13:48:30 +0100

    Minor Fix - Leader Skill bonus
    - Down the bonus from 10% to 5% for each skill levels

--- W40K/Functions/Leader_func.py
+++ W40K/Functions/Leader_func.py
@@ -1,8 +1,8 @@
 
 def set_LeaderSkills(Regiment):
-    Regiment.__SoftAttack *= 1.1**Regiment.Leader.Attack_skill
-    Regiment.__HardAttack *= 1.1**Regiment.Leader.Attack_skill
-    Regiment.__SoftMeleeAttack *= 1.1**Regiment.Leader.Attack_skill
-    Regiment.__HardMeleeAttack *= 1.1**Regiment.Leader.Attack_skill
-    Regiment.__Defense *= 1.1**Regiment.Leader.Defense_skill
-    Regiment.__Breakthrought *= 1.1**Regiment.Leader.Defense_skill
+    Regiment.__SoftAttack *= 1.05**Regiment.Leader.Attack_skill
+    Regiment.__HardAttack *= 1.05**Regiment.Leader.Attack_skill
+    Regiment.__SoftMeleeAttack *= 1.05**Regiment.Leader.Attack_skill
+    Regiment.__HardMeleeAttack *= 1.05**Regiment.Leader.Attack_skill
+    Regiment.__Defense *= 1.05**Regiment.Leader.Defense_skill
+    Regiment.__Breakthrought *= 1.05**Regiment.Leader.Defense_skill
