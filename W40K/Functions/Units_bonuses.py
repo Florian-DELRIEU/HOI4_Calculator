e2901675507f811bd54a29af864cb7da38530385
Florian Delrieu <florian.delrieu@free.fr>
2023-03-19 22:06:07 +0100

    remove W40K folder

--- W40K/Functions/Units_bonuses.py
+++
@@ -1,7 +0,0 @@
-def apply_SpecialRules(unit):
-    for rule in unit.SpecialRules:
-        if rule == "Astartes":
-            unit.org *= 1.2
-        elif rule == "Immobile":
-            unit.Breaktrought = 0 if unit.Type != "Artillery" else 0.8
-            unit.Defense *= 1.2
