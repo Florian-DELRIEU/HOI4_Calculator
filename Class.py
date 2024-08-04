e408db701cb71f03f33bd97df9b8780fe407ed3b
Florian DELRIEU <florian.delrieu@free.fr>
2024-08-05 01:12:45 +0200

    [v 3.1.7] - add get function
    - add get function
    - saving battle for test

--- Class.py
+++ Class.py
@@ -86,3 +86,9 @@ class Terrain:
     def __init__(self, name, width):
         self.name = name
         self.width = width
+
+    def get(self):
+        return {
+            "name": self.name,
+            "width": self.width
+        }
