8020908d1ec7e8faba0956dd586e5db43b8cc142
Florian DELRIEU <florian.delrieu@free.fr>
2024-09-28 02:21:26 +0200

    [3.5.3] - add Constants file

--- Library/Constants.py
+++ Library/Constants.py
@@ -1,8 +1,8 @@
 
 # TACTICS BONUS FOR CAMPS
-COUNTER_TACTIC_MULT = 1.35 # Multiplicateur pour le gagnat de l'initiative dans le choix des contre tactiques
-ATK_INITIATIVE_BONUS = 1 # Bonus d'initiative pour les attaquants
-DEF_INITIATIVE_BONUS = 1 # Bonus d'initiative pour les defenseurs
-RECO_INITIATIVE_BONUS = 5 # Bonus d'initiative si meilleur reco
+COUNTER_TACTIC_MULT = 1.35 # (1.35) Multiplicateur pour le gagnat de l'initiative dans le choix des contre tactiques
+ATK_INITIATIVE_BONUS = 1 # (1) Bonus d'initiative pour les attaquants
+DEF_INITIATIVE_BONUS = 1 # (1) Bonus d'initiative pour les defenseurs
+RECO_INITIATIVE_BONUS = 5 # (5) Bonus d'initiative si meilleur reco
 
-TACTICS_ROUND_OCCURENCE = 12 # Fréquence en rounds pour le choix d'une nouvelle tactique
+TACTICS_ROUND_OCCURENCE = 12 #(12) Fréquence en rounds pour le choix d'une nouvelle tactique
